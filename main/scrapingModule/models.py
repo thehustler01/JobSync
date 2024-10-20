4fdd7d59886a827585e790d9a4ca77c94cb5e2da
Rishabh Vaish <rishabhvaish2002@gmail.com>
2024-10-21 00:52:37 +0530

    job_scraping working properly !!
    except location, job scraping module is fully functional now.

--- main/scrapingModule/models.py
+++ main/scrapingModule/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class JobListing(models.Model):
+    title = models.TextField()  
+    company = models.TextField()  
+    job_link = models.URLField(max_length=1000)  
+    location = models.TextField()  
+    description = models.TextField(null=True, blank=True)  
+    salary = models.TextField(null=True, blank=True) 
+    scraped_for = models.CharField()  
+    scraped_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} at {self.company}"
+
