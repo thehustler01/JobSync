5677011323fa257fca4b5f338fb5a4dbaedd32ad
Rishabh Vaish <rishabhvaish2002@gmail.com>
2025-01-30 18:44:53 +0530

    stable version

--- main/main/settings.py
+++ main/main/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
-
+SECRET_KEY = 'django-insecure-jjt9w80j_^tu$z##fh$$c@4s@+f3^o-@#i_&4sxct_su!7d+v^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -91,11 +90,11 @@ WSGI_APPLICATION = 'main.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DATABASE_NAME'),
-        'USER': os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('DATABASE_HOST'),
-        'PORT': os.getenv('DATABASE_PORT'),
+        'NAME': 'jobsync_db',  
+        'USER': 'jobsync_user',  # PostgreSQL username
+        'PASSWORD': 'tiger',  
+        'HOST': 'localhost',  
+        'PORT': '5432',  
     }
 }
 
