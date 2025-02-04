e5f17525701727e208c36f885872cf3b961209e2
shreya-jain-30 <officialshreyaj2003@gmail.com>
2025-02-05 00:19:49 +0530

    Added Course Recommendation
    Added Course Recommendation based on missing skills

--- main/parsingModule/urls.py
+++ main/parsingModule/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-from .views import upload_resume
+from .views import upload_resume, course_recommend
 
 urlpatterns = [
     path('', upload_resume, name='resumeParser'),
+    path('course-recommend/',course_recommend,name='course_recommend'),
 ]
