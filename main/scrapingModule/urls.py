8e512a6d292a658314f133e306152e5f53028f62
Rishabh Vaish <rishabhvaish2002@gmail.com>
2025-02-15 00:45:00 +0530

    Hiring process details feature added !!

--- main/scrapingModule/urls.py
+++ main/scrapingModule/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import scrape_website ,job_search_without_skill
+from .views import scrape_website ,job_search_without_skill,hiring_process_insights
 
 urlpatterns = [
     path('', scrape_website, name='scrape_website'),  
     path('jobs/',job_search_without_skill,name='job_search_without_skill'),
+    path('hiring_insights/',hiring_process_insights,name='hiring_process_insights'),
 ]
